af2c46288955df043f74b525d7491cc725423799
Marvin Jung <marvinjung@outlook.com>
2026-03-02 22:36:40 +0100

    fill spinner with data

--- app/src/main/java/com/jungma/currencyconverter/MainActivity.java
+++ app/src/main/java/com/jungma/currencyconverter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jungma.currencyconverter;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Spinner spinner_currencyFrom = findViewById(R.id.spinner_currencyFrom);
+        Spinner spinner_currencyTo = findViewById(R.id.spinner_currencyTo);
+        ExchangeRateDatabase exchangeRateDatabase = new ExchangeRateDatabase();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.spinner_view_item, R.id.textview_spinner, exchangeRateDatabase.getCurrencies());
+
+        spinner_currencyFrom.setAdapter(arrayAdapter);
+        spinner_currencyTo.setAdapter(arrayAdapter);
     }
+
+
+
 }
