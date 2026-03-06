65a1cfdcca378b3d65218ea1b7b1a08080826f7b
Marvin Jung <marvinjung@outlook.com>
2026-03-06 16:33:22 +0100

    feat: exchangerate can be edited

--- app/src/main/java/com/jungma/currencyconverter/CurrencyListActivity.java
+++ app/src/main/java/com/jungma/currencyconverter/CurrencyListActivity.java
@@ -3,12 +3,16 @@ package com.jungma.currencyconverter;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,6 +22,7 @@ import java.util.Arrays;
 
 public class CurrencyListActivity extends AppCompatActivity {
     private final ExchangeRateDatabase exchangeRateDatabase = new ExchangeRateDatabase();
+    private ActivityResultLauncher<Intent> editCurrencyLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +40,27 @@ public class CurrencyListActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.currency_list);
         listView.setAdapter(currencyListAdapter);
 
+        editCurrencyLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        //handle returned data here
+                        String currency = data.getStringExtra("currencyName");
+                        double newRateForOneEuro = Double.parseDouble(data.getStringExtra("newRateForOneEuro"));
+                        exchangeRateDatabase.setExchangeRate(currency, newRateForOneEuro);
+                        currencyListAdapter.notifyDataSetChanged();
+                    }
+                }
+        );
+
         listView.setOnItemClickListener((parent, view, position, id) -> {
             String currency = (String) currencyListAdapter.getItem(position);
             Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0`?q=" + exchangeRateDatabase.getCapital(currency)));
-            startActivity(mapIntent);
+            Intent editCurrencyIntent = new Intent(CurrencyListActivity.this, EditCurrencyActivity.class);
+            editCurrencyIntent.putExtra("currencyName", currency);
+            editCurrencyIntent.putExtra("rateForOneEuro", exchangeRateDatabase.getExchangeRate(currency));
+            editCurrencyLauncher.launch(editCurrencyIntent);
         });
     }
 }
---
+++ app/src/main/java/com/jungma/currencyconverter/EditCurrencyActivity.java
@@ -0,0 +1,45 @@
+package com.jungma.currencyconverter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EditCurrencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_currency);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.editCurrency), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText editText = findViewById(R.id.editText_currencyRate);
+        editText.setOnEditorActionListener((v, actionId, event) -> {
+            Log.i("EditorActionListenerTest", "Callback triggered!");
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                Intent returnIntent = new Intent();
+                String currencyName = getIntent().getStringExtra("currencyName");
+                returnIntent.putExtra("currencyName", currencyName);
+                returnIntent.putExtra("newRateForOneEuro", v.getText().toString());
+                setResult(RESULT_OK, returnIntent);
+                finish();
+                return true;
+            }
+            return false;
+        });
+    }
+}
